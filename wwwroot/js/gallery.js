56c6b4502c61b76f81ad21511d13dc1b391f9fb0
Khushnuma31 <kpadaya149@rku.ac.in>
2025-04-03 14:13:08 +0530

    Login register and homepage updates

---
+++ wwwroot/js/gallery.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('gallery', gallery);
+
+    gallery.$inject = ['$location'];
+
+    function gallery($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'gallery';
+
+        activate();
+
+        function activate() { }
+    }
+})();
